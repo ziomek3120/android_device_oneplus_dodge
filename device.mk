86b5e919e55ff0d45fe08e81e99dc7e4b0ef9e9e
chandu078 <chandudyavanapelli03@gmail.com>
2025-06-25 16:32:17 +0000

    dodge: Switch to FIFO vibration effects
    FIFO vibration effect sequences based on dumps from dodge,
    with minor tweaks.
    
    Change-Id: I7a2d9dbc5ea07f99a2284676a49b2c45d2ad3f3b
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
 
 # Vibrator
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
+$(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator/include)
 
 # Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8750-common/common.mk)
