8d9583b3c69f859f2712b001b7ed2ffeda1938fd
Pranav Vashi <neobuddy89@gmail.com>
2025-11-01 17:04:37 +0530

    dodge: Enable UDFPS animations
    * crdroid feature
    
    Change-Id: Ic0c7bfed383d17f7badd7e7fbde6eb83be700369
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946756802996883.xml
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
 
