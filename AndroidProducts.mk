a5db4bafbafda125e9d62953d4e2e569a64a48d8
ziomek3120 <unboxorg@gmail.com>
2026-01-01 23:48:57 +0100

    dodge: Infinity bringup
    Change vendor/lineage to vendor/infinity, add infinity props and flags

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dodge.mk
+    $(LOCAL_DIR)/infinity_dodge.mk
--- infinity_dodge.mk
+++ infinity_dodge.mk
@@ -12,9 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/dodge/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_dodge
+# Maintainer Name
+INFINITY_MAINTAINER := "unboxorg"
+
+# Whether Including Google Apps
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_dodge
 PRODUCT_DEVICE := dodge
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
